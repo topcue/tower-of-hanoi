614e3e0267000535b3a991f1dba981ceb707c940
topcue <topcue1019@naver.com>
2021-10-16 15:09:26 +0900

    Add hanoi.py

---
+++ hanoi.py
@@ -0,0 +1,17 @@
+def hanoi(n, k, a=1, b=2, c=3):
+	if (k == pow(2, n-1)):
+		print("{} {}".format(a, c))
+	elif (k < pow(2, n-1)):
+		hanoi(n-1, k, a, c, b)
+	else:
+		hanoi(n-1, k-pow(2, n-1), b, a, c)
+
+
+if __name__ == "__main__":
+	n = 64
+	k = 18446744073709551615
+	hanoi(n, k)
+
+
+# EOF
+
